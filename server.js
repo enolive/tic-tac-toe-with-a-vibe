04eb57fab640355a90c6e18d17f4846d8ef429e3
Christoph Welcz <christoph.welcz@codecentric.de>
2025-07-20 21:13:14 +0200

    now working!!!

--- server.js
+++ server.js
@@ -3,11 +3,20 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const path = require('path');
+const cors = require('cors');  // Add this line
 
 // Create Express app
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {    // Add CORS configuration for Socket.IO
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+// Enable CORS for Express
+app.use(cors());
 
 // Serve static files
 app.use(express.static(path.join(__dirname, '/')));
